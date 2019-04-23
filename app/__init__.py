eb4fd04108c23d2c9d7e30ddf69ef675ef6163d0
Micah Weiberg <micah.weiberg@gmail.com>
2019-04-23 16:56:37 -0500

    Implementing Jinja into HTML Table
    Using Jinja for loops and if statements to condense HTML table code for creating the 8x8 grid of the chess board. Beginning writing AJAX code for buttons in JS.

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@
 # Version: 04-15-19
 # Class that imports Flask to run the app
 from flask import Flask
+from app.views import TestView
 
 app = Flask(__name__)
 # app.config.from_object('config')
 
-from app.views import TestView
 TestView.register(app, route_base='/')
--- app/static/buttons.js
+++ app/static/buttons.js
@@ -1,23 +1,46 @@
 /**
  * Author: Micah Weiberg
- * Version: 04-18-19
+ * Version: 04-23-19
  * JavaScript file to create the clickable buttons for the chess board
  */
 
+// Defines variable to the table headings with the "black" class for black tiles
 let b = document.getElementsByClassName('black');
+// Defines variable to the table headings with the "white" class for white tiles
 let w = document.getElementsByClassName('white');
+
+// This is in JavaScript format and currently incomplete
+// Needs to be finished in JQuery format
+// function loadDoc(url, callFunction) {
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState === 4 && this.status === 200) {
+//             callFunction(this);
+//         }
+//     };
+//     xhttp.open("POST", url, true);
+//     xhttp.send();
+// }
 
 /**
  * Function that changes the chess board cell color
  * @param color The color the top-leftmost cell is to be changed to
  */
 function buttonClick(color) {
+    /* When the 'Switch Tile Colors' button is pressed ("color = black" button),
+        the tiles (table headings) that are under the "black" class are changed
+         to white and the tiles under the "white" class are changed to black
+     */
     if (color === 'black') {
         for (let i = 0; i < b.length; i++) {
             b[i].style.backgroundColor = 'white';
             w[i].style.backgroundColor = 'black';
         }
     }
+    /* When the 'Return to Original Configuration button is pressed ("color = white" button),
+        the tiles that are under the "white" class are changed to black and the
+        tiles under the "black" class are changed to white
+     */
     else if (color === 'white') {
         for (let i = 0; i < w.length; i++) {
             b[i].style.backgroundColor = 'black';

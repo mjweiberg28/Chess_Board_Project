c0c92bb543b1ba5727535d3dc443dc2487727116
Micah Weiberg <micah.weiberg@gmail.com>
2019-05-15 16:42:27 -0500

    JQuery POST to Python
    Adding JQuery code in JS file to send a POST request with data to the __init__.py file where data is taken in and the board is rendered based upon the data received. Reverting back to having 2 buttons instead of 1.

--- app/static/buttons.js
+++ app/static/buttons.js
@@ -1,31 +1,27 @@
 /**
  * Author: Micah Weiberg
- * Version: 05-02-19
- * JavaScript file to create the clickable button for the chessboard
+ * Version: 05-15-19
+ * JavaScript file to create the clickable buttons for the chessboard
  */
-
-/**
- * Variable that keeps track of whether the button has been clicked, changing the
- * orientation of the chessboard grid colors
- * @type {boolean}
- */
-let clicked = true;
 
 /**
  * JQuery using AJAX and JS to change the color of the chessboard upon
- * clicking the button. Each button click changes the colors of the
- * 8x8 grid and sends a POST request to display a pop-up that a button
- * was successfully clicked
+ * clicking the buttons. Each button click sends a POST request to
+ * views/__init__.py to change the colors of the 8x8 chessboard grid
  */
 $(document).ready(function() {
-    $("#button").click(function() {
-        input_data = {}
-        // If-else statements to change the .white and .black CSS classes between black and white
-        $('.white').css('background-color', clicked ? 'black' : 'white');
-        $('.black').css('background-color', clicked ? 'white' : 'black');
-        clicked = !clicked;
-        $.post("/buttonClick", input_data, function(output_data){
-            alert(output_data);
-        });
+    // When "Switch Tile Colors" button is clicked, send POST to View with clicked = true
+    $("#black-button").click(function() {
+        let input_data = {'clicked': true}
+         $.post('/buttonClick', input_data, function(output_data) {
+             document.getElementById('board').innerHTML = output_data;
+         });
+    });
+    // When "Return to Original" button is clicked, send POST to View with clicked = false
+    $("#white-button").click(function() {
+        let input_data = {'clicked': false}
+        $.post('/buttonClick', input_data, function(output_data) {
+            document.getElementById('board').innerHTML = output_data;
+         });
     });
 });
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,17 +1,22 @@
 # Author: Micah Weiberg
-# Version: 04-25-19
+# Version: 05-15-19
 # Class that imports render_template to run on a web-page, works as a View class for MVC
 from flask import render_template, request
 from flask_classy import FlaskView, route
 
 
 class TestView(FlaskView):
-
+    # Method that initially renders the index.html class
     @route('/')
     def index(self):
+        # Render HTML with 'index.html' to initialize the chessboard
         return render_template('index.html', **locals())
 
+    # Method for the POST request from buttons.js
     @route('/buttonClick', methods=['post'])
-    def buttonClick(self):
-        return 'Chessboard color has been changed!'
-
+    def click(self):
+        # Set data equal to the value for 'clicked' from buttons.js
+        data = request.form.get('clicked')
+        # Render HTML with 'board.html' and colors dependent on whether
+        # data = 'true' or 'false', allowing the board to change colors
+        return render_template('board.html', data=data)
